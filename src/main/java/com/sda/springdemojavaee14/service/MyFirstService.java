b8664bdc8df7350f65121191ab3bf5005c6969fa
Kristaps <sebriskristaps@gmail.com>
2022-06-18 14:02:58 +0300

    [change] - typo in line 5

--- src/main/java/com/sda/springdemojavaee14/service/MyFirstService.java
+++ src/main/java/com/sda/springdemojavaee14/service/MyFirstService.java
@@ -2,7 +2,7 @@ package com.sda.springdemojavaee14.service;
 
 import org.springframework.stereotype.Service;
 
-@Service //@Service - for bussinges logic
+@Service //@Service - for business logic
 public class MyFirstService {
 
 }
