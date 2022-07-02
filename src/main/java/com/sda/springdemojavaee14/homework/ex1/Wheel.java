11fc24e88cec715edad3c83bf1aae2a092c8469c
Kristaps Sebris <sebriskristaps@gmail.com>
2022-07-02 11:10:29 +0300

    [change] - using @Scope with prototype to provide new Wheel every time its needed

--- src/main/java/com/sda/springdemojavaee14/homework/ex1/Wheel.java
+++ src/main/java/com/sda/springdemojavaee14/homework/ex1/Wheel.java
@@ -1,12 +1,16 @@
 package com.sda.springdemojavaee14.homework.ex1;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+//Singleton creates only one instance, but to create four wheel, we have to use @Scope annotation and change from
+// singleton scope to scope prototype
+@Component("ritenis")
+@Scope(SCOPE_PROTOTYPE)
 public class Wheel {
 
     //constructor
-
     public Wheel() {
         System.out.println("Creating new wheel");
     }
