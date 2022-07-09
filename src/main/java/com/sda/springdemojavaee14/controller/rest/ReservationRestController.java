6e8d9f37de203f7325011543e9cef674b05e69dc
Kristaps Sebris <sebriskristaps@gmail.com>
2022-07-09 10:54:58 +0300

    [change] - moving dummy data from ReservationRestController to ReservationService

--- src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
@@ -1,6 +1,7 @@
 package com.sda.springdemojavaee14.controller.rest;
 
 import com.sda.springdemojavaee14.entity.Reservation;
+import com.sda.springdemojavaee14.service.ReservationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,35 +15,16 @@ import java.util.List;
 @RequestMapping("/api")
 public class ReservationRestController {
 
+    private final ReservationService reservationService;
+
+    public ReservationRestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
     @GetMapping("/reservations")
     public List<Reservation> getAllReservations() {
-        log.info("getting all reservations");
-        return List.of(
-                //1 approach
-                /*
-                new Reservation(1L,
-                        12L,
-                        LocalDateTime.now(),
-                        LocalDateTime.now().plusHours(2),
-                        "Liepins",
-                        "+371 4444440",
-                        "asd@qwe.com",
-                        4,
-                        "Latvia, Riga"),
-                */
+        log.info("getting all reservations (Controller)");
 
-                //2nd approach
-                Reservation.builder()
-                        .id(1L)
-                        .tableNumber(10L)
-                        .startBookingTime(LocalDateTime.now())
-                        .endBookingTime(LocalDateTime.now().plusHours(2))
-                        .surname("Sebris")
-                        .phoneNumber("+371 22222222")
-                        .email("sebris@kristaps.com")
-                        .numberOfPeople(5)
-                        .address("Latvia, Ogre")
-                        .build()
-        );
+        return reservationService.findAllReservations();
     }
 }
