9f971fbb6991c25d512b5c61a3e73795aa010dc4
sebronas <sebriskristaps@gmail.com>
2022-07-16 13:28:24 +0300

    [add] - finding reservation by id - quite good approach (status 200 and 404)

--- src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
@@ -3,6 +3,8 @@ package com.sda.springdemojavaee14.controller.rest;
 import com.sda.springdemojavaee14.entity.Reservation;
 import com.sda.springdemojavaee14.service.ReservationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,8 +33,17 @@ public class ReservationRestController {
 
     @GetMapping("/reservations/{id}")
                                   //@PathVariable("id") get id value from url and use for reservationId
-    public Reservation getReservationById(@PathVariable("id") Long reservationId) {
+    public ResponseEntity<Reservation> getReservationById(@PathVariable("id") Long reservationId) {
         log.info("trying to find reservation by id: [{}]", reservationId);
-        return reservationService.findReservationById(reservationId);
+
+        //return reservationService.findReservationById(reservationId);
+        var responseBody = reservationService.findReservationById(reservationId);
+       /*     ResponseEntity.status(HttpStatus.OK)
+                    .body(responseBody);*/
+        ResponseEntity<Reservation> result = ResponseEntity.notFound().build();
+        if (responseBody != null) {
+            result = ResponseEntity.ok(responseBody);
+        }
+        return result;
     }
 }
